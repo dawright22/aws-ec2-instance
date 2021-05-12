ac1936a685fb86f148f052ae95e3eeb9ede3dc59
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-05-12 15:55:40 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,24 +6,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
   owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = ami-0d53808c8c345ed07 #data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   tags = {
