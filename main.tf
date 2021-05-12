563799c983fbcedde8300a5519e2a0db1dbaf52d
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-05-12 15:36:07 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,16 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  #availability_zone = "${var.aws_region}a"
+  availability_zone = data.aws_availability_zones.available.names
 
-  tags {
-    Name = "${var.name}"
+  tags = {
+    Name = "HelloWorld"
   }
 }
