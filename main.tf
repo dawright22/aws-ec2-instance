1e8c2c25461de47b4a6e52ea21a4dedafda796e9
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-05-12 15:47:39 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,30 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.14.0"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  #availability_zone = data.aws_availability_zones.available.names
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
